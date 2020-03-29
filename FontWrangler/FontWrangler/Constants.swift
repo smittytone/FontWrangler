19e4970c86807c0a56724c5ec25fdf91f6117d69
smittytone <smittytone@icloud.com>
2020-03-29 13:23:34 +0100

    Add constants file

---
+++ FontWrangler/FontWrangler/Constants.swift
@@ -0,0 +1,15 @@
+
+//  Created by Tony Smith on 29/03/2020.
+//  Copyright © 2020 Tony Smith. All rights reserved.
+
+
+import Foundation
+
+// MARK: Constants
+
+let kFontsDirectoryPath     = "/fonts"
+let kFontListFileSubPath    = "/fonts/.fontlist"
+
+let kDeregisterFontTimeout  = 10.0
+
+let kFontSampleText_1       = "ABCDEFGHI\nJKLMNOPQ\nRSTUVWXYZ\n\n0123456789\n\nabcdefghi\njklmnopq\nrstuvwxyz\n\n!@£$%^&~*()[]{}"
