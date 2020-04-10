1385eb249f0a6cc56e42a56005373a131457f569
smittytone <smittytone@icloud.com>
2020-04-10 12:51:04 +0100

    Always re-set the view outline path

--- FontWrangler/FontWrangler/UserTestSampleView.swift
+++ FontWrangler/FontWrangler/UserTestSampleView.swift
@@ -1,31 +1,26 @@
-//
-//  UserTestSampleView.swift
-//  FontWrangler
-//
+
 //  Created by Tony Smith on 10/04/2020.
 //  Copyright © 2020 Tony Smith. All rights reserved.
-//
+
 
 import UIKit
 
-class UserTestSampleView: UIView {
 
-    private var path: UIBezierPath?
+class UserTestSampleView: UIView {
 
     override func draw(_ rect: CGRect) {
 
-        let newRect = rect.insetBy(dx: 2.0, dy: 2.0)
-        // Drawing code
-        if self.path == nil {
-            path = UIBezierPath(roundedRect: newRect, cornerRadius: 12.0)
-            path!.lineWidth = 2.0
-        }
-
+        // Get the current graphics context to set the stroke colour
         if let c = UIGraphicsGetCurrentContext() {
             c.setStrokeColor(UIColor.label.cgColor)
         }
 
-        path!.stroke(with: .normal, alpha: 0.7)
+        // Inset the view rect by two pixels - the path's width - and
+        // create a bezier as the context's stroke path
+        let newRect = rect.insetBy(dx: 2.0, dy: 2.0)
+        let path = UIBezierPath(roundedRect: newRect, cornerRadius: 12.0)
+        path.lineWidth = 2.0
+        path.stroke(with: .normal, alpha: 0.7)
     }
 
 }
