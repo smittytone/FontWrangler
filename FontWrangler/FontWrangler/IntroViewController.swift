f1407c5fbe135ca4331bb4be2ee17cbb1dbe0a94
smittytone <smittytone@icloud.com>
2020-04-12 13:15:01 +0100

    Correct intro view text colour

--- FontWrangler/FontWrangler/IntroViewController.swift
+++ FontWrangler/FontWrangler/IntroViewController.swift
@@ -10,6 +10,25 @@ class IntroViewController: UIViewController {
 
     // Subclass UIViewController in order to add the close button
 
+    @IBOutlet weak var ownTextView: UITextView!
+
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Update the embedded Attributed String with the correct colour,
+        // unobtainable via InterfaceBuilder
+        let mas: NSMutableAttributedString = NSMutableAttributedString(attributedString: self.ownTextView.attributedText)
+        let range = NSRange(location: 0,
+                            length: mas.length)
+        mas.addAttribute(NSAttributedString.Key.foregroundColor,
+                         value: UIColor.label,
+                         range: range)
+        self.ownTextView.attributedText = mas
+    }
+
+
     @IBAction func doClose(_ sender: Any) {
 
         // Close the Help panel
