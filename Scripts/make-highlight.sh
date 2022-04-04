e6b494cd0d70d75ca4f226043370642f31e633fa
Tony Smith <smittytone@icloud.com>
2022-04-04 12:25:11 +0100

    Make a bash script

--- Scripts/make-highlight.sh
+++ Scripts/make-highlight.sh
@@ -1,13 +1,13 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Additional langauges required
 additions=(applescript coffeescript ada brainfuck delphi clojure fortran arduino vbscript lisp erlang elixir protobuf armasm latex)
 
 # REQUIRES highlight.js clone in your $GIT directory
-if cd $GIT/highlight.js ; then
+if cd "$GIT/highlight.js" ; then
     if node tools/build.js -t browser :common ${additions} ; then
-        if [[ -e build/highlight.min.js ]]l then
-            cp build/highlight.min.js $GIT/HighlighterSwift/Sources/Assets/highlight.min.js
+        if [[ -e build/highlight.min.js ]]; then
+            cp build/highlight.min.js "$GIT/HighlighterSwift/Sources/Assets/highlight.min.js"
         else
             echo '[ERROR] highlight.min.js not built'
         fi
